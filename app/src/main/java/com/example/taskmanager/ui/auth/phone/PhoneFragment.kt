d6b8eaa96de5f583af35b01e3ab45fedebf03f61
Farhhd <zazasihamid081@gmail.com>
2024-01-11 18:02:43 +0600

    homeWork_7

--- app/src/main/java/com/example/taskmanager/ui/auth/phone/PhoneFragment.kt
+++ app/src/main/java/com/example/taskmanager/ui/auth/phone/PhoneFragment.kt
@@ -1,14 +1,17 @@
 package com.example.taskmanager.ui.auth.phone
 
+import android.R
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.example.taskmanager.R
 import com.example.taskmanager.databinding.FragmentPhoneBinding
 import com.example.taskmanager.utils.showToast
 import com.google.firebase.FirebaseException
@@ -17,6 +20,7 @@ import com.google.firebase.auth.PhoneAuthCredential
 import com.google.firebase.auth.PhoneAuthOptions
 import com.google.firebase.auth.PhoneAuthProvider
 import java.util.concurrent.TimeUnit
+
 
 class PhoneFragment : Fragment() {
 
@@ -36,9 +40,13 @@ class PhoneFragment : Fragment() {
             verificationId: String,
             token: PhoneAuthProvider.ForceResendingToken,
         ) {
-            findNavController().navigate(R.id.verifyFragment, bundleOf(VER_ID_KEY to verificationId))
+            findNavController().navigate(
+                com.example.taskmanager.R.id.verifyFragment,
+                bundleOf(VER_ID_KEY to verificationId)
+            )
         }
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -49,8 +57,36 @@ class PhoneFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.etPhone.setText(getString(R.string._996))
-        binding.btnSend.setOnClickListener{
+
+        binding.etPhone.setText("+7")
+
+        binding.etPhone.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(
+                charSequence: CharSequence,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+            }
+
+            override fun onTextChanged(
+                charSequence: CharSequence,
+                start: Int,
+                before: Int,
+                count: Int
+            ) {
+                if (start == 0 && count < before) {
+
+                    binding.etPhone.setText("+996")
+                    binding.etPhone.text?.let { binding.etPhone.setSelection(it.length) }
+                }
+            }
+
+            override fun afterTextChanged(editable: Editable) {
+
+            }
+        })
+        binding.btnSend.setOnClickListener {
             val options = PhoneAuthOptions.newBuilder(FirebaseAuth.getInstance())
                 .setPhoneNumber(binding.etPhone.text.toString())
                 .setTimeout(60L, TimeUnit.SECONDS)
@@ -60,7 +96,8 @@ class PhoneFragment : Fragment() {
             PhoneAuthProvider.verifyPhoneNumber(options)
         }
     }
-    companion object{
-        const val  VER_ID_KEY = "ver.id.key"
+
+    companion object {
+        const val VER_ID_KEY = "ver.id.key"
     }
 }
